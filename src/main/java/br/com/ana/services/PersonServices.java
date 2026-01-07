a230db64d2680a106790c77a680a84047ff886ca
Ana Paula Carvalho Caetano <78127576+AnaPaula-CarvalhoCaetano@users.noreply.github.com>
2026-01-07 14:20:00 -0300

    Update PersonServices.java
    organização do service.

--- src/main/java/br/com/ana/services/PersonServices.java
+++ src/main/java/br/com/ana/services/PersonServices.java
@@ -21,7 +21,6 @@ import java.util.concurrent.atomic.AtomicLong;
 @Service
 public class PersonServices {
 
-    private final AtomicLong counter = new AtomicLong();
     private Logger logger = LoggerFactory.getLogger(PersonServices.class.getName());
 
 
